27d6640c820e3d9cc1c3b6311a6b2392b64a2c66
dustpancake <fergusbkr@gmail.com>
2020-08-13 20:43:02 +0200

    Created AES config header file stub, for reference in my notes repository.

---
+++ src/utility/ECCX08DefaultAESConfig.h
@@ -0,0 +1,11 @@
+/*
+ * stub file created for future implementation, and for immediate use in my notes references
+ *
+ * Created 13 August 2020 by Fergus Baker
+ */
+
+#ifndef _ECCX08_DEFAULT_AES_CONFIG_H_
+#define_ECCX08_DEFAULT_AES_CONFIG_H_
+
+
+#endif
